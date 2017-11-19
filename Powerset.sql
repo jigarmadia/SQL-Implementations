a464aafec78f7312d2213f49f0655ef62b6a40f4
jigarmadia <33801229+jigarmadia@users.noreply.github.com>
2017-11-19 17:40:29 -0500

    Powerset Final

--- Powerset.sql
+++ Powerset.sql
@@ -1,5 +1,6 @@
 
-CREATE TABLE A(x integer, PRIMARY KEY(x));
+CREATE TABLE A(x integer, 
+               PRIMARY KEY(x));
 CREATE TABLE A_Powerset(xa integer[]);
 
 INSERT INTO A VALUES(1);
@@ -12,7 +13,11 @@ INSERT INTO A VALUES(5);
 CREATE OR REPLACE FUNCTION merge_array_value(x integer, xa anyarray)
  RETURNS anyarray AS 
  $$
- 	SELECT ARRAY( SELECT x FROM ( ( SELECT x ) UNION ( SELECT UNNEST(xa) AS x) ) a ORDER BY x );
+ 	SELECT ARRAY( SELECT x 
+                  FROM ( ( SELECT x ) 
+                   UNION ( SELECT UNNEST(xa) AS x) 
+                       ) a 
+                ORDER BY x );
  $$ LANGUAGE sql;
 
 --Creates powerset values from the uniary table A
@@ -20,37 +25,44 @@ CREATE OR REPLACE FUNCTION generate_powerset()
   RETURNS void AS
   $$
   	DECLARE row_count integer;
-  			            i	integer;
+  			              i	integer;
   	BEGIN
 
   		--Insert null into powerset
   		INSERT INTO A_Powerset VALUES('{}');
 
   		--Get original row count
-  		SELECT INTO row_count count(1) FROM A;
+  		SELECT INTO row_count count(1) 
+      FROM A;
 
-      --The new row_count will be the number of times we need to multiply the row_count cardinality values with original values
+    --The new row_count will be the number of times we need to multiply the row_count cardinality values with original values
   		row_count := row_count - 1;
 
   		FOR i IN 0..row_count
   		 LOOP
-
   		 	WITH 
         --Original entries from table A
-  		 	original_entries AS ( SELECT a.x FROM A a ),
+  		 	  original_entries AS ( SELECT a.x FROM A a ),
+        
         --Powerset entries with row_count cardinality
-  		 	powerset_entries AS ( SELECT ap.xa FROM A_Powerset ap
-  		 						   WHERE CARDINALITY(ap.xa) = i ),
+  		 	  powerset_entries AS ( SELECT ap.xa 
+                                FROM A_Powerset ap
+  		 						                    WHERE CARDINALITY(ap.xa) = i 
+                             ),
+                             
         --New row combinations by multiplying original with powerset_entries of cardinality i 
         --We dont consider combinations where original entry is already in the multiplying powerset entry
         --and original entries which are greater than any of the elements in the multiplying powerset collection
         --to maintain unique combinations 
-  		 	new_rows AS ( SELECT o.x, p.xa 
-  		 			        FROM original_entries o, powerset_entries p
-  		 			   	   WHERE NOT ( o.x = SOME(p.xa) )
-  		 			   	 	 AND o.x < ALL(p.xa) )
+  		 	  new_rows AS ( SELECT o.x, p.xa 
+  		 			                FROM original_entries o, powerset_entries p
+  		 			   	           WHERE NOT ( o.x = SOME(p.xa) )
+  		 			   	 	               AND o.x < ALL(p.xa) 
+                     )
+                     
         --Merge possible combinations in one array and add to powerset table
-  		 	INSERT INTO A_Powerset SELECT merge_array_value(nr.x,nr.xa) FROM new_rows nr;
+  		 	  INSERT INTO A_Powerset SELECT merge_array_value(nr.x,nr.xa) 
+                                 FROM new_rows nr;
 
   		 END LOOP;
   	END
